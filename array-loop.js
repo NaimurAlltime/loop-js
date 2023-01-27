3d86e2a2d02051d78a1433c13b9893df28554a54
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 23:56:15 +0600

    arry loop in js

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+var numbers = [10, 20, 22, 26, 28, 32, 40];
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['alu', 'potol', 'kumra', 'morich', 'badha-kopi'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
