2699df1a95cdb2d51ce8b4588f5f2e5c68509fad
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 23:39:53 +0600

    for loop in js

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+// 1 to 10 odd number 
+for(var i = 1; i <= 10; i +=2){
+    console.log(i);
+}
+// 0 to 10 even number 
+for(var i = 0; i <= 10; i +=2){
+    console.log(i);
+}
