fb8dcf0ec1a50cde3c34dd19a64d11b3221b09cd
Naimur Rahman <naimur2231@gmail.com>
2023-01-27 23:20:27 +0600

    odd and even number while loop

---
+++ odd-even.js
@@ -0,0 +1,14 @@
+// 1 to 20 odd number 
+var number = 1;
+while(number <= 20){
+    console.log(number);
+    number += 2;
+}
+
+
+// 0 to 20 even number 
+number = 0;
+while(number <= 20){
+    console.log(number);
+    number += 2;
+}
